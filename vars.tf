610143d6f380ce4ca7fbeaff82c8356c8175af71
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2021-01-20 15:42:48 +0000

    adding descriptions to variables

--- vars.tf
+++ vars.tf
@@ -1,12 +1,12 @@
+variable "approle_name" {
+  type        = string
+  description = "(Required) The name of the AppRole."
+}
+
 variable "policies_path" {
   type        = string
   default     = ""
-  description = "Absolute path to directory containing Vault policies"
-}
-
-variable "approle_name" {
-  type        = string
-  description = "The name of the role."
+  description = "(Optional) The absolute path to directory containing Vault policies. If not set, it defaults to current working directory."
 }
 
 variable "role_id" {
@@ -18,13 +18,13 @@ variable "role_id" {
 variable "approle_backend_path" {
   type        = string
   default     = null
-  description = "The path that the AppRole backend is mounted at."
+  description = "(Optional) The unique name of the auth backend to configure. Defaults to `approle`."
 }
 
 variable "bind_secret_id" {
   type        = bool
   default     = true
-  description = "(Optional) Whether or not to require secret_id to be presented when logging in using this AppRole. Defaults to true."
+  description = "(Optional) Whether or not to require `secret_id` to be presented when logging in using this AppRole. Defaults to `true`."
 }
 
 variable "secret_id_bound_cidrs" {
@@ -48,25 +48,25 @@ variable "secret_id_ttl" {
 variable "token_bound_cidrs" {
   type        = list(string)
   default     = null
-  description = ""
+  description = "(Optional) List of CIDR blocks; if set, specifies blocks of IP addresses which can authenticate successfully, and ties the resulting token to these blocks as well."
 }
 
 variable "token_explicit_max_ttl" {
   type        = number
   default     = null
-  description = ""
+  description = "(Optional) If set, will encode an explicit max TTL onto the token in number of seconds. This is a hard cap even if `token_ttl` and `token_max_ttl` would otherwise allow a renewal."
 }
 
 variable "token_max_ttl" {
   type        = number
   default     = null
-  description = ""
+  description = "(Optional) The maximum lifetime for generated tokens in number of seconds. Its current value will be referenced at renewal time."
 }
 
 variable "token_no_default_policy" {
   type        = bool
   default     = false
-  description = ""
+  description = "(Optional) If set, the default policy will not be set on generated tokens; otherwise it will be added to the policies set in token_policies."
 }
 
 variable "token_num_uses" {
@@ -78,23 +78,23 @@ variable "token_num_uses" {
 variable "token_period" {
   type        = number
   default     = null
-  description = ""
+  description = "(Optional) If set, indicates that the token generated using this role should never expire. The token should be renewed within the duration specified by this value. At each renewal, the token's TTL will be set to the value of this field. Specified in seconds."
 }
 
 variable "token_ttl" {
   type        = number
   default     = null
-  description = ""
+  description = "(Optional) The maximum lifetime for generated tokens in number of seconds. Its current value will be referenced at renewal time."
 }
 
 variable "token_type" {
   type        = string
   default     = "service"
-  description = ""
+  description = "(Optional) The type of token that should be generated. Can be service, batch, or default to use the mount's tuned default (which unless changed will be service tokens). For token store roles, there are two additional possibilities: default-service and default-batch which specify the type to return unless the client requests a different type at generation time."
 }
 
 variable "additional_policies" {
   type        = list(string)
   default     = []
-  description = "A list of any additional policies to add to the AppRole."
+  description = "(Optional) A list of any additional policies to add to the AppRole. These policies must already exist in Vault."
 }
